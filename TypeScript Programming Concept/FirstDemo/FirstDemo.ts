c9d18172d6904d566adc291ab2ad663207a05502
MahbubulRishad <mahbubulrishad@gmail.com>
2025-09-04 01:48:37 +0600

    Environment setup and print data

---
+++ TypeScript Programming Concept/FirstDemo/FirstDemo.ts
@@ -0,0 +1,13 @@
+// // For Installing Typescript: 
+
+// node js
+// install typescript - npm install -g typescript
+// check version: tsc --version
+
+
+// // Install TSX (Typescript executor) - to run Typescript directly 
+// command: npm install -g tsx
+
+
+
+console.log("Welcome to Typescript");

5afb8b0c3c2b9384b2944c3754eaba96dbcea35a
MahbubulRishad <mahbubulrishad@gmail.com>
2025-09-04 12:21:33 +0600

    Comemnting

---
+++ TypeScript Programming Concept/Variables/Comments.ts
@@ -0,0 +1,15 @@
+// Single line Comment -> Ctrl + /
+// Multiple line Comment -> Shift + Alt + A
+
+// console.log("Single Line comment");
+
+
+
+/* console.log("Multi-line Comment");
+console.log("Multi-line Commentt");
+console.log("Multi-line Comment");
+console.log("Multi-line Comment");
+console.log("Multi-line Comment");
+console.log("Multi-line Comment"); */
+
+
